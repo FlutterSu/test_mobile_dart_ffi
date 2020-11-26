85c92c04629ea0ea0d2964eb8d35a9fa00915893
Egor <ead@tvip.ru>
2020-11-26 12:40:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# native_add
+
+Test mobile dart:ffi
+
+cpp lib code: [ios/Classes/native_add.cpp](ios/Classes/native_add.cpp)
+
+dart lib code: [lib/native_add.dart](lib/native_add.dart)
+
+flutter examle code: [example/lib/main.dart](example/lib/main.dart)
---
+++ example/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:native_add/native_add.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('1 + 2 == ${nativeAdd(1, 2)}'),
+        ),
+      ),
+    );
+  }
+}
